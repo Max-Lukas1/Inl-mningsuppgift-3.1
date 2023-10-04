a5e6f5928f47c3dfa7cd42717abe613a2143cae2
majo06002 <Max-Lukas@outlook.com>
2023-10-04 10:22:06 +0200

    Add project files.

---
+++ Inlämningsuppgift 3.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace inlämningsuppgift3_1
+{
+    class program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            string x = Console.ReadLine();
+            switch(x)
+            {
+                case "16":
+                    Console.WriteLine("Du får vara med");
+                break;
+                case "17":
+                    Console.WriteLine("Du får vara med");
+                    break;
+                case "18":
+                    Console.WriteLine("Du får vara med");
+                    break;
+                 default: 
+                    Console.WriteLine("tyvärr får du inte vara med");
+                    break;
+               
+            }
+
+        }
+    }
+}
